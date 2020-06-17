36a1dbecdb97bdcb9e44af52159a3dddb55b4f5e
Jose lopez <lopezajoseg@gmail.com>
2020-06-17 11:54:22 -0400

    build

---
+++ build/precache-manifest.ad7730d5418e9f51bb425aaced9e36e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb9e786c12f92095f09c",
+    "url": "/static/css/main.067de0ac.chunk.css"
+  },
+  {
+    "revision": "bb9e786c12f92095f09c",
+    "url": "/static/js/main.bb9e786c.chunk.js"
+  },
+  {
+    "revision": "77fcc32944b6874eb862",
+    "url": "/static/js/1.77fcc329.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "053570b07af7363cda3e963de5f47b2c",
+    "url": "/index.html"
+  }
+];
